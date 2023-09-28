08471db18dc9765063545fe9291ee25adac8cdae
ShubhanginiJadhav <51948536+ShubhanginiJadhav@users.noreply.github.com>
2023-09-28 11:13:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+
+iconClose.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
+})
